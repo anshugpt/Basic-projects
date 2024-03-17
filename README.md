021f039dedd30dd85a81d5529a4d38accda034f0
anshugpt <138896567+anshugpt@users.noreply.github.com>
2024-03-17 11:21:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic-projects
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/anshugpt/Basic-projects)
---
+++ Todo Appliction/app.js
@@ -0,0 +1,27 @@
+const userInput = document.querySelector('input');
+const addBtn = document.querySelector('#add-btn');
+const taskBar = document.querySelector('#show-task');
+
+function createTaskBar(value) {
+  const p = document.createElement('p');
+
+  p.innerHTML = value;
+  taskBar.appendChild(p);
+}
+
+function validation() {
+  if (userInput.value == '') {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+addBtn.addEventListener('click', function () {
+  if (validation()) {
+    createTaskBar(userInput.value);
+    userInput.value = '';
+  } else {
+    alert = 'Enter the correct task';
+  }
+});
